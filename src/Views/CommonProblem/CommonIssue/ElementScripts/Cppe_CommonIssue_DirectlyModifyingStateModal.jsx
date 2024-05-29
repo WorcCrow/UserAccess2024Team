41361e15f641cdfd3a69610cbc1365c1dd1929ac
LeoNguyen <leo.nguyen@salary.com>
2024-05-29 12:32:07 +0700

    Resolve all common issue

--- src/Views/CommonProblem/CommonIssue/ElementScripts/Cppe_CommonIssue_DirectlyModifyingStateModal.jsx
+++ src/Views/CommonProblem/CommonIssue/ElementScripts/Cppe_CommonIssue_DirectlyModifyingStateModal.jsx
@@ -2,37 +2,43 @@ import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
 export const DirectlyModifyingStateModal = ({ HideModal }) => {
-   const [intCounter, SetCounter] = useState(10);
-   const [objData, SetData] = useState({ counter: 20 });
+  const [intCounter, SetCounter] = useState(10);
+  const [objData, SetData] = useState({ counter: 20 });
 
-   const incrementCounter = () => {
-      intCounter += 1;
-      console.log(intCounter);
-   };
+  const incrementCounter = () => {
+    // intCounter += 1;
+    // console.log(intCounter);
+    SetCounter((intCounter) => intCounter + 1); // correct way to update state
+  };
 
-   const incrementObjectCounter = () => {
-      objData.counter += 1;
-      console.log(objData.counter);
-   }
+  const incrementObjectCounter = () => {
+    //  objData.counter += 1;
+    //   console.log(objData.counter);
+    SetData((prev) => ({ ...prev, counter: prev.counter + 1 })); // correct way to update object
+  };
 
-   return (
-      <Modal show={true} onHide={HideModal} centered>
-         <Modal.Header closeButton>
-            <Modal.Title>Directly Modifying State</Modal.Title>
-         </Modal.Header>
-         <Modal.Body>
-            <div className="mb-3">
-               <Button variant="primary" onClick={incrementCounter}>Increment {intCounter}</Button>
-            </div>
-            <div>
-               <Button variant="success" onClick={incrementObjectCounter}>Increment Object Counter {objData.counter}</Button>
-            </div>
-         </Modal.Body>
-         <Modal.Footer>
-            <Button variant="secondary" onClick={HideModal}>
-               Close
-            </Button>
-         </Modal.Footer>
-      </Modal>
-   )
-}
+  return (
+    <Modal show={true} onHide={HideModal} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Directly Modifying State</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="mb-3">
+          <Button variant="primary" onClick={incrementCounter}>
+            Increment {intCounter}
+          </Button>
+        </div>
+        <div>
+          <Button variant="success" onClick={incrementObjectCounter}>
+            Increment Object Counter {objData.counter}
+          </Button>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={HideModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
